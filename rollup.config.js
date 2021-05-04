609f74314d9d544bc6e510352f75dcbd4802b96d
GrahamSH <grahamshllk@gmail.com>
2021-05-04 12:13:42 +0000

    Big changes

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,15 @@
 import resolve from '@rollup/plugin-node-resolve';
-
+import { terser } from "rollup-plugin-terser";
 export default {
   input: 'src/color-picker.js',
-  output: {
+  output: [{
     file: 'dist/color-picker-esm.js',
     format: 'esm'
   },
+  {
+    file: 'dist/color-picker-esm.min.js',
+    format: 'esm',
+    plugins: [terser()]
+  }],
   plugins: [ resolve()]
 };
